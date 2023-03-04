4607521aedac6b9eb2c0cecb607b4eaeca8fa952
BohdanHromchak <gromchakbogdan@gmail.com>
2023-03-04 09:10:30 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,33 @@ import Filter from './Filter/Filter';
 import Layout from './Layout/Layout';
 const { Component } = require("react");
 
+const initialContacts = [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+{id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+{id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+{id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},]
 
 class App extends Component {
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: initialContacts,
     filter: ''
   }
+
+componentDidMount() {
+const savedContacts = localStorage.getItem('contacts')
+if(savedContacts !== null) {
+  const parsedContacts = JSON.parse(savedContacts)
+  this.setState({contact: parsedContacts})
+  return
+}
+this.setState({contacts: initialContacts})
+}
+
+componentDidUpdate(prevProps, prevState) {
+if(prevState.contacts !== this.state.contacts){
+  localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+}
+}
+
 
 formSubmitHandler = (data) => {
 
